27f98b599d6c88678651d6786b9d7752cb9a0d77
applekraken <ldlwbru0218@gmail.com>
2018-11-25 18:32:54 +0900

    commit Queue

--- DataStructure/Queue.py
+++ DataStructure/Queue.py
@@ -18,12 +18,38 @@ def GetReadlineData():
 
 def DataPros(threstime, prosname, prostime):
 
+    lenpros = len(prosname)
     endtime = sum(prostime)
     nowprostime = 0
+    i = 0
+    endcnt = 0
+
     while nowprostime <= endtime:
+        endcnt = endcnt + 1
+        if endcnt > 100:
+            print("エラー発生")
+            break
+        if prostime[i] > threstime:
+            nowprostime = nowprostime + 100
+            prostime[i] = prostime[i] - 100
+        else:
+            nowprostime = nowprostime + prostime[i]
+            print("{} {}".format(prosname[i], prostime[i]))
+            prostime.remove[i+1]
+            prosname.remove[i+1]
+            lenpros = lenpros - 1
+
+        if i < lenpros:
+            i = i + 1
+        else:
+            i = 0
+    
         
 
 def main():
+    [threstime, prosname, prostime] = GetReadlineData()
+    DataPros(threstime, prosname, prostime)
+
 
 if __name__ == '__main__':
     main()
