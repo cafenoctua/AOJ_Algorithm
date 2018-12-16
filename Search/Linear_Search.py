b59bb194d250d9e7ed546c41cb3a460920a29489
Unknown <ldlwbru0218@gmail.com>
2018-12-16 16:52:39 +0900

    Linnear_Search2

--- Search/Linear_Search.py
+++ Search/Linear_Search.py
@@ -1,39 +1,35 @@
 import sys
 
 def InputData():
-    # inputslen = int(sys.stdin.readline().rstrip())
-    # inputs= sys.stdin.readline()
 
-    # sortvalue = [int(i) for i in inputs.split()]
+    data_len = int(input())
+    data = input().split()
     
-    # readlines = sys.stdin.readline()
 
-    deta_len = int(sys.stdin.readline().rstrip())
-    datas = sys.stdin.readline()
-    data = [int(i) for i in datas.split()]
+    searchdata_len = int(input())
+    searchdata = input().split()
+    
+    data = int(data)
+    searchdata = int(searchdata)
 
-    searchdata_len = int(sys.stdin.readline().rstrip())
-    searchdatas = sys.stdin.readline()
-    searchdata = [int(i) for i in searchdatas.split()]
-
-    return deta_len, data, searchdata_len, searchdata
+    return data_len, data, searchdata_len, searchdata
 
 def Liner_Search(data_len, data, searchdata_len, searchdata):
 
     contained_cnt = 0
     for i in range(searchdata_len):
         for j in range(data_len):
-            if searchdata[i] == data[j]:
+            if data[j] == searchdata[i]:
                 contained_cnt = contained_cnt + 1
     
     return contained_cnt
 
 def PrintOut(contained_cnt):
-    print(contained_cnt)
+    print(int(contained_cnt))
 
 def main():
-    [deta_len, data, searchdata_len, searchdata] = InputData()
-    contained_cnt = Liner_Search(deta_len, data, searchdata_len, searchdata)
+    [data_len, data, searchdata_len, searchdata] = InputData()
+    contained_cnt = Liner_Search(data_len, data, searchdata_len, searchdata)
     PrintOut(contained_cnt)
 
 if __name__=='__main__':
